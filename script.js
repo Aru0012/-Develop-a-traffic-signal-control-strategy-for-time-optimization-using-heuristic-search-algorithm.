855ed485d123d75d12bb673b78ea18698e9d1d6b
Aru0012 <150217080+Aru0012@users.noreply.github.com>
2024-02-13 18:09:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function redirectToQuestion() {
+    window.location.href = "question.html";
+  }
+  
+  function displayMessage(answer) {
+    if (answer === 'yes') {
+      window.location.href = "happy.html";
+    }
+  }
+  
